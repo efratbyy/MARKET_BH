3d736bad62c540b865f1d5dda119e2580ded6cbc
efrat ben yosef <efratbyy@gmail.com>
2023-10-30 17:23:26 +0200

    Add search filter to mobile shopping cart

--- client/src/cart/ShoppingCartMobilePage.tsx
+++ client/src/cart/ShoppingCartMobilePage.tsx
@@ -1,23 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../navbar/Navbar";
 import ShoppingCart from "./ShoppingCart";
 import Footer from "../footer/Footer";
-import { Button, Grid, Paper, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Divider,
+  Grid,
+  Input,
+  Paper,
+  Typography,
+} from "@mui/material";
 import { useCartProvider } from "../providers/CartProvider";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import ROUTES from "../routes/routesModel";
+import { CartProductInterface } from "../models/interfaces/interfaces.ts";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+import { useUser } from "../providers/UserProvider";
 
 const ShoppingCartMobilePage = () => {
-  const { cart } = useCartProvider();
+  const { cart, updateCartProvider, updateCartNoteProvider } =
+    useCartProvider();
   const navigate = useNavigate();
+  const { user } = useUser();
 
   const [totalAmountInCart, setTotalAmountInCart] = React.useState<number>(0);
+  const [query, setQuery] = useState<string>("");
+  const [searchParams] = useSearchParams();
+
+  const handleNoteChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    barcode: string
+  ): void => {
+    if (user)
+      updateCartNoteProvider(user?._id, barcode, event.target.value.toString());
+  };
 
   useEffect(() => {
-    if (cart)
+    const query = searchParams.get("q");
+    if (query != null) {
+      setQuery(query);
+    } else {
+      setQuery("");
+    }
+  }, [searchParams]);
+
+  useEffect(() => {
+    if (cart) {
       setTotalAmountInCart(
         cart.reduce((number, item) => number + item.amount, 0)
       );
+    }
   }, [cart]);
   return (
     <>
@@ -52,8 +89,142 @@ const ShoppingCartMobilePage = () => {
           </Paper>
         </Grid>
       </Grid>
-      <ShoppingCart />
+      {/* <ShoppingCart /> */}
+      <Box sx={{ height: "60%" }}>
+        {cart
+          ?.filter((item) => item.title.includes(query))
+          .map((item: CartProductInterface, index) => (
+            <div key={item.barcode}>
+              <Grid
+                container
+                sx={{ marginBottom: "3%", marginTop: "3%", fontSize: "90%" }}
+              >
+                <Grid
+                  item
+                  xs={2}
+                  md={2}
+                  container
+                  alignContent="center"
+                  alignItems="center"
+                  justifyContent="center"
+                  padding={"3%"}
+                >
+                  <ButtonGroup
+                    orientation="vertical"
+                    variant="contained"
+                    sx={{
+                      width: "100%",
+                      boxShadow: 0, // Set the desired width to make it smaller
+                    }}
+                  >
+                    <Button
+                      onClick={() =>
+                        updateCartProvider(user!._id, item.barcode, -1)
+                      }
+                    >
+                      <RemoveIcon />
+                    </Button>
+                    <Button disabled>
+                      <Typography variant="body1">{item.amount}</Typography>
+                    </Button>
+                    <Button
+                      onClick={() =>
+                        updateCartProvider(user!._id, item.barcode, 1)
+                      }
+                    >
+                      <AddIcon />
+                    </Button>
+                  </ButtonGroup>
+                </Grid>
 
+                <Grid
+                  item
+                  xs={2}
+                  md={2}
+                  sx={{
+                    textAlign: "center", // Center horizontally
+                    display: "flex",
+                    alignItems: "center", // Center vertically
+                    justifyContent: "center", // Center horizontally
+                  }}
+                >
+                  <div>
+                    <img
+                      src={item.image.url}
+                      alt={item.title}
+                      style={{
+                        maxWidth: "100%",
+                        width: "85%",
+                        height: "auto",
+                      }}
+                    />
+                  </div>
+                </Grid>
+
+                <Grid item xs={4} md={4}>
+                  <div style={{ marginTop: "10%" }}>{item.brand}</div>
+                  <div style={{ marginTop: "10%" }}>{item.title}</div>
+                  <div style={{ marginTop: "10%" }}>
+                    <Input
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        handleNoteChange(e, item.barcode)
+                      } // Handle input changes
+                      value={item.note}
+                      placeholder="הוסף הערה:"
+                      sx={{
+                        border: "1px solid rgba(0,0,0,0.3)",
+                        borderRadius: "2px",
+                      }}
+                      // onKeyDown={(e) => {
+                      //   if (e.key === " ") {
+                      //     e.preventDefault(); // Prevent the default behavior of the space key
+                      //   }
+                      // }}
+                    ></Input>
+                  </div>
+                </Grid>
+                <Grid
+                  item
+                  xs={2}
+                  md={2}
+                  sx={{
+                    textAlign: "center", // Center horizontally
+                    display: "flex",
+                    alignItems: "center", // Center vertically
+                    justifyContent: "center", // Center horizontally
+                  }}
+                >
+                  ₪{item.price.toFixed(2)}
+                </Grid>
+                <Grid
+                  item
+                  xs={2}
+                  md={2}
+                  sx={{
+                    textAlign: "center", // Center horizontally
+                    display: "flex",
+                    alignItems: "center", // Center vertically
+                    justifyContent: "center", // Center horizontally
+                  }}
+                >
+                  <Button
+                    onClick={() =>
+                      updateCartProvider(
+                        user!._id,
+                        item.barcode,
+                        item.amount * -1
+                      )
+                    }
+                  >
+                    <DeleteIcon sx={{ color: "#ce0a0a" }} />
+                  </Button>
+                </Grid>
+                {/* <div className={`weight ${item.isProductOutOfStock ? 'disabled' : ''}`}>{item.product.weight}</div> */}
+              </Grid>
+              {index < cart.length - 1 && <Divider variant="middle" />}
+            </div>
+          ))}
+      </Box>
       <Grid
         container
         sx={{
