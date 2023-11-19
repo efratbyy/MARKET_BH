31c57d64cfe098f0298b7cf9d3c15389440d64ef
efrat ben yosef <efratbyy@gmail.com>
2023-11-19 10:25:50 +0200

    hide creditCard number in email

--- client/src/apiService/emailApiService.ts
+++ client/src/apiService/emailApiService.ts
@@ -82,9 +82,12 @@ export const emailPaymentDetailsApi = async (
         user_name: userName,
         user_email: userEmail,
         card_holder_name: cardHolderName,
-        card_number: cardNumber,
+        card_number: cardNumber.replace(
+          /(\d{4} \d{4} \d{4}) (\d{4})/,
+          "xxxx xxxx xxxx $2"
+        ),
         card_expiration_date: cardExpirationDate,
-        card_cvv: cardCvv,
+        card_cvv: cardCvv.replace(/\d{3}/g, "xxx"),
         card_holder_id: cardHolderId,
         cart: htmlCart,
         order_number: orderNumber,
@@ -99,100 +102,3 @@ export const emailPaymentDetailsApi = async (
     return Promise.reject("An unexpected error occurred!");
   }
 };
-
-// export const emailToClientApi = async (
-//   date: Date,
-//   userName: string,
-//   userEmail: string,
-//   cardHolderName: string,
-//   cardNumber: string,
-//   cardExpirationDate: string,
-//   cardCvv: string,
-//   cardHolderId: string,
-//   cart: CartProductInterface[] | undefined,
-//   orderNumber: Number | undefined
-// ) => {
-//   try {
-//     const emailImage = `<img src="https://cdn.pixabay.com/photo/2012/10/15/18/47/habaneros-61505_1280.jpg" alt="Image Alt Text" style="max-width: 100%;">
-//     <table style="font-family: Arial, sans-serif; border-collapse: collapse; width: 100%;">`;
-//     let htmlCart = "";
-//     // convert cart to an HTML table
-//     if (cart === undefined || cart?.length === 0) {
-//       htmlCart = "Empty Cart";
-//     } else {
-//       // Create the table header with inline styles
-//       const tableHeader = `
-//         <tr>
-//         <th style="background-color: #4CAF50; color: white; text-align: center; padding: 8px;">הערות</th>
-//         <th style="background-color: #4CAF50; color: white; text-align: center; padding: 8px;">מותג</th>
-//         <th style="background-color: #4CAF50; color: white; text-align: center; padding: 8px;">ברקוד</th>
-//         <th style="background-color: #4CAF50; color: white; text-align: center; padding: 8px;">מחיר</th>
-//         <th style="background-color: #4CAF50; color: white; text-align: center; padding: 8px;">כמות</th>
-//         <th style="background-color: #4CAF50; color: white; text-align: center; padding: 8px;">פריט</th>
-//         </tr>
-//       `;
-
-//       // Map over the cart items to create rows
-//       const tableRows = cart
-//         ?.map(
-//           (item) => `
-//             <tr>
-//             <td style="border: 1px solid #dddddd; text-align: center; padding: 8px;">${
-//               item.note
-//             }</td>
-//             <td style="border: 1px solid #dddddd; text-align: center; padding: 8px;">${
-//               item.brand
-//             }</td>
-//             <td style="border: 1px solid #dddddd; text-align: center; padding: 8px;">${
-//               item.barcode
-//             }</td>
-//             <td style="border: 1px solid #dddddd; text-align: center; padding: 8px;">₪${item.price.toFixed(
-//               2
-//             )}</td>
-//               <td style="border: 1px solid #dddddd; text-align: center; padding: 8px;">${
-//                 item.amount
-//               }</td>
-//               <td style="border: 1px solid #dddddd; text-align: center; padding: 8px;">${
-//                 item.title
-//               }</td>
-
-//             </tr>
-//           `
-//         )
-//         .join(""); // Join the rows into a single string
-
-//       // Create the complete HTML table as a string
-//       htmlCart = `
-//       <table style="direction:ltr;">
-//           ${tableHeader}
-//           ${tableRows}
-//         </table>
-//       `;
-//     }
-
-//     const { data } = await axios.post<string>(`${apiUrl}/api/v1.0/email/send`, {
-//       service_id: "service_biwkfjt",
-//       template_id: "template_l268em2",
-//       user_id: "mQ1xbiPBiYd0g3bMK",
-//       template_params: {
-//         date: date,
-//         user_name: userName,
-//         user_email: userEmail,
-//         card_holder_name: cardHolderName,
-//         card_number: cardNumber,
-//         card_expiration_date: cardExpirationDate,
-//         card_cvv: cardCvv,
-//         card_holder_id: cardHolderId,
-//         cart: htmlCart,
-//         order_number: orderNumber,
-//         image: emailImage,
-//       },
-//     });
-//     return Promise.resolve(data);
-//   } catch (error) {
-//     if (axios.isAxiosError(error)) {
-//       console.log(error);
-//     }
-//     return Promise.reject("An unexpected error occurred!");
-//   }
-// };
