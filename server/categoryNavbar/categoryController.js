f11af4b81c0c51126cadbd8f0be15006cd8fb7da
efrat ben yosef <efratbyy@gmail.com>
2023-11-27 14:48:51 +0200

    remove unnecessary files

--- server/categoryNavbar/categoryController.js
+++ server/categoryNavbar/categoryController.js
@@ -4,7 +4,6 @@ const { handleError } = require("../utils/handleErrors");
 const getCategories = async (req, res) => {
   try {
     const categoriesFromDB = await BigCategory.find();
-    console.log(categoriesFromDB);
     return res.send(categoriesFromDB);
   } catch (error) {
     return handleError(res, 404, `Mongoose Error: ${error.message}`);
